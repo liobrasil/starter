6cba917be411ac3920e59a2c3ce2e7e14cd1892c
Lionel LIMOL <limol.lionel@gmail.com>
2022-02-17 12:42:23 -0400

    Update account script

--- src/cadence/scripts/account-info.script.js
+++ src/cadence/scripts/account-info.script.js
@@ -1,8 +1,14 @@
 export const ACCOUNT_INFO = `import FlowStorageFees from 0xServiceAccount
     pub fun main(address: Address): {String: AnyStruct} {
+
+        // Return Flow from Storage (byte)
+        fun returnFlowFromStorage(_ storage: UInt64): UFix64 {
+            let f = UFix64(storage % 100000000 as UInt64) * 0.00000001 as UFix64 + UFix64(storage / 100000000 as UInt64)
+            let storageMb = f * 100.0 as UFix64
+            let storage = FlowStorageFees.storageCapacityToFlow(storageMb)
+            return storage
+        }
+
     let account = getAccount(address)
-    let storageMb = UFix64(account.storageCapacity/1000000)
-    let storage2Flow = FlowStorageFees.storageCapacityToFlow(storageMb)
-    let flow2storage = FlowStorageFees.flowToStorageCapacity(storage2Flow)
-    return {"balance": account.balance, "availableBalance": account.availableBalance, "storageUsed": account.storageUsed/1000, "storageCapacity": storageMb,  "storage2Flow": storage2Flow, "flow2Storage":flow2storage} 
+    return {"balance": account.balance, "availableBalance": account.availableBalance, "storageUsed": account.storageUsed, "storageCapacity": account.storageCapacity} 
 }`;
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,11 +5,8 @@ export default function Summary({ accountInfos }) {
     <Flex direction="column">
       <Text>Balance ($Flow): {accountInfos?.balance}</Text>
       <Text>Available Balance ($Flow): {accountInfos?.availableBalance}</Text>
-      <Text>Storage Used (kB): {accountInfos?.storageUsed}</Text>
-      <Text>Storage Capacity (MB): {accountInfos?.storageCapacity}</Text>
-      ------------------------------------------
-      <Text>Storage2Flow ($FLOW): {accountInfos?.storage2Flow}</Text>
-      <Text>Flow2Storage (MB): {accountInfos?.flow2Storage}</Text>
+      <Text>Storage Used (bytes): {accountInfos?.storageUsed}</Text>
+      <Text>Storage Capacity (bytes): {accountInfos?.storageCapacity}</Text>
     </Flex>
   );
 }
